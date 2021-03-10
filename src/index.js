4f93764c18f135a4c45cecde82cf2e317b3c38ae
Mantuok <etigiloff@gmail.com>
2021-03-10 23:24:12 +0300

    init: brackets done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2 !== 0) return false;
+
+  const isPariedBracketsInString = (str, bracketsConfig) => {
+    let pairedBracketList = [];
+    for(let elem of bracketsConfig){
+      pairedBracketList.push(elem[0] + elem[1]);
+    }
+    //['()', '[]', '{}', '||'];
+    for (let pair of pairedBracketList) {
+      if (str.indexOf(pair) !== -1) return [true, pair];
+    }
+    return [false, ''];
+  }
+
+  while (str.length > 0) {
+    let a = isPariedBracketsInString(str, bracketsConfig);
+    if (!a[0]) return false;
+    str = str.slice(0, str.indexOf(a[1])) + str.slice(str.indexOf(a[1]) + 2);
+  }
+  return true;
 }
